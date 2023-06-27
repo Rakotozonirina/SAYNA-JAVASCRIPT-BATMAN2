bcb8fd3179fbd01c8519e7c377980b80b3cee4c5
Zo Nirina <tomzonirina@gmail.com>
2023-06-27 09:35:32 +0300

    I adding the four animation

--- Pages/index.js
+++ Pages/index.js
@@ -1,8 +1,24 @@
-gsap.from(".who-batman", {
-    x: 50,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".cinema-batman",
-        markers: {colorStart: "white", colorEnd: "white"}
-    }
+gsap.to(".contenu-text > p", {
+    scrollTrigger: ".hero",
+    yoyo: true,
+    keyframes: [{ duration:2, x:50, opacity: 0}, {duration:2, x:0, opacity:1}],
+    delay: 1
+});
+gsap.to(".who-batman", {
+    scrollTrigger: ".cinema-batman",
+    yoyo: true,
+    keyframes: [{ duration:2, x:50, opacity: 0}, {duration:2, x:0, opacity:1}],
+    delay: 4
+});
+gsap.to(".who-nemesis", {
+    scrollTrigger: ".nemesis",
+    yoyo: true,
+    keyframes: [{ duration:2, x:50, opacity: 0}, {duration:2, x:0, opacity:1}],
+    delay: 8
+});
+gsap.to(".who-allies", {
+    scrollTrigger: ".allies",
+    yoyo: true,
+    keyframes: [{ duration:2, x:50, opacity: 0}, {duration:2, x:0, opacity:1}],
+    delay: 12
 });
