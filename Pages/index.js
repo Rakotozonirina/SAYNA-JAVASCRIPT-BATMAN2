9093c172bacf9cbf27b3b28cd3e460f94a17e4be
Zo Nirina <tomzonirina@gmail.com>
2023-06-27 17:55:49 +0300

    I have finish the first animation cards

--- Pages/index.js
+++ Pages/index.js
@@ -27,4 +27,31 @@ gsap.to(".who-justice-league", {
     yoyo: true,
     keyframes: [{ duration:2, x:50, opacity: 0}, {duration:2, x:0, opacity:1}],
     delay: 16
-})
+});
+
+const cardPattinson = document.getElementById("Pattinson");
+const contentPattinson = document.getElementById("content-pattinson");
+cardPattinson.addEventListener('mouseover', () => {
+    gsap.to(contentPattinson, {duration:2, visibility: 'visible', opacity: 1});
+});
+cardPattinson.addEventListener('mouseout', () => {
+    gsap.to(contentPattinson, {duration:2, visibility: 'hidden', opacity: 0});
+});
+
+const cardChristian = document.getElementById("Christian");
+const contentChristian = document.getElementById("content-christian");
+cardChristian.addEventListener('mouseover', () => {
+    gsap.to(contentChristian, {duration:2, visibility: 'visible', opacity: 1});
+});
+cardChristian.addEventListener('mouseout', () => {
+    gsap.to(contentChristian, {duration:2, visibility: 'hidden', opacity: 0});
+});
+
+const cardBen = document.getElementById("Ben");
+const contentBen = document.getElementById("content-ben");
+cardBen.addEventListener('mouseover', () => {
+    gsap.to(contentBen, {duration:2, visibility: 'visible', opacity: 1});
+});
+cardBen.addEventListener('mouseout', () => {
+    gsap.to(contentBen, {duration:2, visibility: 'hidden', opacity: 0});
+});
